3fad279b12d93e6a24aa40ef09037c2aa7f93640
Magischername <Magischername@gmail.com>
2018-04-11 00:45:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'd!help') {
-        message('Hier ist eine Liste der Befehle');
+        message.channel.send('Hier ist eine Liste der Befehle');
     }
 });
 
