1992aa19398ef40105479d791e8d1d685ae3e560
Magischername <Magischername@gmail.com>
2018-04-11 15:18:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,8 +13,10 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'd!help') {
-        message.channel.send(' ```Markdown #Liste der Befehle ``` ');
-        message.channel.send(' ```Basic Befehle: d!test ``` ');
+        message.channel.send(' ```~~Liste der Befehle~~ ``` ');
+        message.channel.send(' ``` Befehle müssen mit einem d! anfangen.``` ');
+        message.channel.send(' ``` 1. Basics - help``` ');
+        message.channel.send(' ``` 2. Platzhalter ``` ');
     }
 });
 
