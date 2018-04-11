3e369c4ccac5ec290823c0efb2ddce4c939a074e
Magischername <Magischername@gmail.com>
2018-04-11 20:20:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json");
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(config.token);
+
